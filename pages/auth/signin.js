678d4957f218f35053c5d9827e82e3c4dd7b1444
Vinh <73513296+VinnyBui@users.noreply.github.com>
2021-10-23 13:38:56 -0700

    Updated

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,15 +3,24 @@ import{ getProviders, signIn as signIntoProvider} from "next-auth/react";
 function signIn({providers}) {
   return (
     <>
-      <h1> sign in pages</h1>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIntoProvider(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
     </>
   );
 }
 
 export async function getServerSideProps(){
   const providers = await getProviders();
+
   return {
-    props: { providers },
+    props: { 
+      providers, 
+    },
   };
 }
 
