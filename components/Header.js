78976c553d4b698194b3caf33e8ab6bcdec883e1
Vinh <73513296+VinnyBui@users.noreply.github.com>
2021-11-06 15:34:36 -0700

    Implemented signin

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,25 @@
 import Image from "next/image";
 import {
-  SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon, HomeIcon,
+  SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+import { signIn, signOut, useSession} from "next-auth/react";
 
 function Header() {
+  const { data : session } = useSession();
+
+
+
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50 ">
       <div className ="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/*left */}
-        <div className ="relative w-24 cursor-pointer">
-          <Image src="/ful.png" layout="fill" objectFit="contain"/>
+        <div className ="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image 
+            src="/ful.png" 
+            layout="fill" 
+            objectFit="contain"
+          />
         </div>
 
         {/*center - Search bar */}
@@ -26,16 +36,35 @@ function Header() {
         <div className="flex items-center justify-end space-x-4">
         <HomeIcon className="navBtn"/>
         <MenuIcon className="h-6 md:hidden cursor-pointer"/>
-        <PaperAirplaneIcon className="navBtn"/>
-        <PlusCircleIcon className="navBtn"/>
-        <UserGroupIcon className="navBtn"/>
-        <HeartIcon className="navBtn"/>
-        </div>
         
+        {session ? (
+          <>
+            <div className="relative navBtn">
+              <PaperAirplaneIcon className="navBtn rotate-45" />
+              <div className="absolute -top-1 -right-2 text-xs w-5
+              h-5 bg-red-500 rounded-full flex items-center
+              justify-center animate-pulse text-white">
+                3
+              </div>
+            </div>
+            <PlusCircleIcon className="navBtn"/>
+            <UserGroupIcon className="navBtn"/>
+            <HeartIcon className="navBtn"/>
 
+            <img
+              onClick={signOut}
+              src={session.user.image}
+              alt="profile pic"
+              className="h-10 w-10 rounded-full cursor-pointer"
+            />
+          </> 
+        ) : (
+          <button onClick={signIn}>Sign In</button>
+        )}
       </div>
     </div>
-  )
+  </div>
+  );
 }
 
-export default Header
+export default Header;
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,32 @@
 import{ getProviders, signIn as signIntoProvider} from "next-auth/react";
+import Header from "../../components/Header";
 
 function signIn({providers}) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+
+      <div className='flex flex-col items-center justify-center
+      min-h-screen py-2 -mt-56 px-14 text-center'>
+        <img className="w-80" src="http://www.sunimprint.com/images/Cal%20State%20Fullerton%20Titans%201992-2009%20Wordmark%20Logo%20diy%20iron%20on%20transfers%203.png"
+        alt="" />
+        <p className=" font-xs">
+          Welcome to TitanTalks! This is our version of an Instagram Clone!
+        </p>
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button
+              className="p-3 bg-blue-500 rounded-lg text-white"
+              onClick={() => signIntoProvider(provider.id, { callbackUrl: "/"})}
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
@@ -19,7 +36,7 @@ export async function getServerSideProps(){
 
   return {
     props: { 
-      providers, 
+      providers,
     },
   };
 }
